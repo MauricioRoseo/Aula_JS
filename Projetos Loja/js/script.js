11e3dd05902b7d7bdf95ba41d3a2f253989fc47b
MauricioRoseo <mauriciornn@gmail.com>
2023-05-09 10:41:14 -0300

    atualização 09/05

--- Projetos Loja/js/script.js
+++ Projetos Loja/js/script.js
@@ -5,6 +5,9 @@ const cs = (el) => document.querySelectorAll(el);
 modeloJson.map((item, index)=>{
     let modeloItem = c('.models .modelo-item').cloneNode(true);
 
+    modeloItem.setAttribute('data-key', index);
+
+
     modeloItem.querySelector('.modelo-item-img img').src = item.img;
     modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
     modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
@@ -13,8 +16,21 @@ modeloJson.map((item, index)=>{
     modeloItem.querySelector('a').addEventListener('click', (e) =>{
         e.preventDefault();
 
+        let key = e.target.closest(".modelo-item").getAttribute('data-key');
+
+        c(".modeloBig img").src = modeloJson[key].img
+        c(".modeloInfo h1").innerHTML = modeloJson[key].name
+        c(".modeloInfo .modeloInfo-desc").innerHTML = modeloJson[key].description
+        c(".modeloInfo .modeloInfo-pricearea .modeloInfo-actualPrice").innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`;
+
+        c(".modeloWindowArea").style.opacity = 0;
         c('.modeloWindowArea').style.display = 'flex';
+        setTimeout(() =>{
+            c('.modeloWindowArea').style.opacity = 1;
+        }, 200)
     });
+
+    
 
     c('.modelo-area').append(modeloItem);
 })
