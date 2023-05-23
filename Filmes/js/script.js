01c9f6b74cb795f96c106a088f95fe8e3aee0be9
MauricioRoseo <mauriciornn@gmail.com>
2023-05-23 10:55:43 -0300

    desenvolvido em 23/05

--- Filmes/js/script.js
+++ Filmes/js/script.js
@@ -2,7 +2,7 @@ const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
 FilmJson.map((item, index)=>{
-    let filmeItem = c(".Content .filme-item").cloneNode(true);
+    let filmeItem = c(".modelo .filme-item").cloneNode(true);
 
     filmeItem.setAttribute('data-key', index);
 
@@ -10,4 +10,6 @@ FilmJson.map((item, index)=>{
     filmeItem.querySelector('.filme-item-name').innerHTML = item.name;
     filmeItem.querySelector('.filme-item-nota').innerHTML = item.notaimdb;
     filmeItem.querySelector('.filme-item-sinopse').innerHTML = item.sinopse;
+
+    c('.modelo-area').append(filmeItem);
 })
