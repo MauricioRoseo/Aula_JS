8b71047291b505868b8f34fac6a55c1fd4e666e3
MauricioRoseo <mauriciornn@gmail.com>
2023-05-23 11:21:53 -0300

    desenvolvido em 23/05

--- Filmes/js/script.js
+++ Filmes/js/script.js
@@ -11,5 +11,20 @@ FilmJson.map((item, index)=>{
     filmeItem.querySelector('.filme-item-nota').innerHTML = item.notaimdb;
     filmeItem.querySelector('.filme-item-sinopse').innerHTML = item.sinopse;
 
+    filmeItem.querySelector('.bnt-linktree').addEventListener('click', (e) =>{
+        e.preventDefault();
+
+        let key = e.target.closest(".modelo-item").getAttribute('data-key');
+
+        c(".assitir-bnt a .trailer-link").href = BDD[key].link_trailer
+        c(".assitir-bnt a .film-link").href = BDD[key].link_film
+
+        c(".linktree").style.opacity = 0;
+        c('.linktree').style.display = 'flex';
+        setTimeout(() =>{
+            c('.linktree').style.opacity = 1;
+        }, 100)
+    });
+
     c('.modelo-area').append(filmeItem);
 })
